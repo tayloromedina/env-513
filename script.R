d6421da43d1f2a4b49163c3f752393263747b17f
Taylor Oran Medina <taylor.medina@epfl.ch>
2023-10-25 11:47:48 +0200

    pca plots

--- script.R
+++ script.R
@@ -5,6 +5,8 @@ library(tidyverse)
 library(here)
 library(readxl)
 library(janitor)
+library(partykit)
+
 
 
 # Load data
@@ -15,3 +17,27 @@ calenviroscreen <- read_excel(here('data', 'calenviroscreen.xlsx')) %>%
 # I succeed in implement R and github -Junjia
 library(ggplot2)
 #hi
+
+
+
+# subset data to only numeric
+subsetdata <- calenviroscreen %>% 
+  select(!1:7) %>% 
+  mutate_if(is.character,as.numeric)
+
+subsetdata <- subsetdata[is.finite(rowSums(subsetdata)),]
+  
+# pca
+pca_fit <- subsetdata %>%  # retain only numeric columns
+  scale() %>% # scale data
+  prcomp()
+
+pca_fit %>%
+  broom::augment(subsetdata) %>% # add original dataset back in
+  ggplot(aes(.fittedPC1, .fittedPC2)) + 
+  geom_point(size = 1.5) 
+
+
+ 
+
+
